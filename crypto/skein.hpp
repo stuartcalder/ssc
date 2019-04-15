8dc2b7c9be77f9580506e45f0cd0601bb26fdf37
Stuart <notstuart@gmail.com>
2019-04-15 00:02:01 -0700

    begin work on Skein

--- crypto/skein.hpp
+++ crypto/skein.hpp
@@ -9,8 +9,25 @@ class Skein
 public:
   static constexpr const size_t State_Bytes = Block_Bits / 8;
 
+  Skein(ThreeFish<Block_Bits> &&tf);
 private:
-  //Internal state
+  void UBI(const uint64_t * const starting_val,
+           const uint8_t  * const message,
+           const uint8_t  * const tweak);
 };
+
+
+template< size_t Block_Bits >
+Skein<Block_Bits>::Skein(ThreeFish<Block_Bits> &&tf)
+  : threefish{ tf }
+{
+}
+template< size_t Block_Bits >
+void Skein<Block_Bits>::UBI(const uint64_t * const starting_val,
+                            const uint64_t * const message,
+                            const  uint8_t * const tweak)
+{
+
+}
 
 #endif
