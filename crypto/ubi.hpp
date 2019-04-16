e389909df3c1fcf06a0d7372299248f3a604e8bd
Stuart <notstuart@gmail.com>
2019-04-15 17:49:21 -0700

    starting on ubi

---
+++ crypto/ubi.hpp
@@ -0,0 +1,13 @@
+#ifndef UBI_HPP
+#define UBI_HPP
+
+template< typename Tweakable_Block_Cipher_t,
+          size_t   State_Size >
+void UBI(const uint64_t * const init,
+         const uint8_t  * const message,
+         const uint64_t * const starting_tweak)
+{
+
+}
+
+#endif
