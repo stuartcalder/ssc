60ca3394547f5406716f54f837a2dec7da905f81
Stuart <notstuart@gmail.com>
2019-04-17 21:58:35 -0700

    remove interface/

--- interface/arguments.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef ARGUMENTS_HPP
-#define ARGUMENTS_HPP
-
-bool sanitize_arguments(const char **arguments,
-                        const char **allowed_args,
-                        const int num_args,
-                        const int num_allowed_args);
-
-#endif
--- interface/ui.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef UI_HPP
-#define UI_HPP
-
-
-
-#endif
