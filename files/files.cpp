d20cbff997a8fa7adf08b8d26e9c4ecc5611033f
Stuart <notstuart@gmail.com>
2019-04-29 12:42:00 -0700

    code for checking to see if a file exists

--- files/files.cpp
+++ files/files.cpp
@@ -9,12 +9,23 @@ size_t get_file_size(std::FILE *stream)
   return num_bytes;
 }
 
+bool file_exists(const char * filename)
+{
+  bool exists = false;
+  std::FILE * test = std::fopen( filename, "rb" );
+  if( test != nullptr ) {
+    std::fclose( test );
+    exists = true;
+  }
+  return exists;
+}
+
 void check_file_name_sanity(const std::string & str,
                             const size_t min_size)
 {
   if( str.size() < min_size ) {
     std::fprintf( stderr, "Error: Filename %s must have at least %zu character(s)\n",
                   str.c_str(), min_size );
-    exit( EXIT_FAILURE );
+    std::exit( EXIT_FAILURE );
   }
 }
--- files/files.hpp
+++ files/files.hpp
@@ -4,5 +4,6 @@
 #include <string>
 
 size_t get_file_size         (std::FILE *stream);
+bool   file_exists           (const char * filename);
 void   check_file_name_sanity(const std::string & str,
                               const size_t min_size);
