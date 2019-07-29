af4d58de5fad9a2dec050576ff28d2ae1dd68aca
Stuart <notstuart@gmail.com>
2019-07-28 19:24:58 -0700

    include windows.h

--- crypto/operations.cc
+++ crypto/operations.cc
@@ -5,6 +5,8 @@
     // Include unistd.h for getentropy()
     #include <unistd.h>
 #elif defined(_WIN64)
+    // Include as the base for win32 crap
+    #include <Windows.h>
     // Include for definitions of NTSUCCESS
     #include <ntdef.h>
     // Include bcrypt.h for BCryptGenRandom()
@@ -20,7 +22,7 @@ namespace ssc
         using namespace std;
 
 #if   defined(__gnu_linux__)
-        static constexpr auto const & Fail_String = "Failed to getentropy()";
+        static constexpr auto const & Fail_String = "Failed to getentropy()\n";
         size_t offset = 0;
         while ( num_bytes >= 256 ) {
             if ( getentropy( (buffer + offset), 256 ) != 0 ) {
@@ -38,25 +40,33 @@ namespace ssc
         BCRYPT_ALG_HANDLE cng_provider_handle;
         // Open algorithm provider
         if ( BCryptOpenAlgorithmProvider( &cng_provider_handle, L"RNG", NULL, 0 ) != STATUS_SUCCESS ) {
-            fputs( "BCryptOpenAlgorithmProvider() failed", stderr );
+            fputs( "BCryptOpenAlgorithmProvider() failed\n", stderr );
             exit( EXIT_FAILURE );
         }
         // Generate randomness
         if ( BCryptGenRandom( cng_provider_handle, buffer, num_bytes, 0 ) != STATUS_SUCCESS ) {
-            fputs( "BCryptGenRandom() failed", stderr );
+            fputs( "BCryptGenRandom() failed\n", stderr );
             exit( EXIT_FAILURE );
         }
         // Close algorithm provider
         if ( BCryptCloseAlgorithmProvider( cng_provider_handle, 0 ) != STATUS_SUCCESS ) {
-            fputs( "BCryptCloseAlgorithmProvider() failed", stderr );
+            fputs( "BCryptCloseAlgorithmProvider() failed\n", stderr );
             exit( EXIT_FAILURE );
         }
+#else
+    #error "ssc::generate_random_bytes only defined for Gnu/Linux and MS Windows"
 #endif
     } /* ! generate_random_bytes */
     void zero_sensitive(void *buffer, std::size_t num_bytes)
     {
         using namespace std;
 
+#if   defined(__gnu_linux__)
         explicit_bzero( buffer, num_bytes );
+#elif defined(_WIN64)
+        SecureZeroMemory( buffer, num_bytes );
+#else
+    #error "ssc::zero_sensitive defined for Gnu/Linux and MS Windows"
+#endif
     }
 } /* ! namespace ssc */
