7f9ef02773436e0ea1f6e296ab10e08dcb694871
root <root@sourcery.home>
2019-08-07 01:23:53 -0700

    export the error-related functions

--- general/error_conditions.hh
+++ general/error_conditions.hh
@@ -1,9 +1,10 @@
 #pragma once
 #include <cstdlib>
 #include <cstdio>
+#include <ssc/general/symbols.hh>
 
 namespace ssc
 {
-    void die_fprintf(char const * die_message);
-    void die_fputs(char const * die_message);
+    DLL_PUBLIC void die_fprintf(char const * die_message);
+    DLL_PUBLIC void die_fputs(char const * die_message);
 }
