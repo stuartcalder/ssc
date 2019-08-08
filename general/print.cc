34d1570b24bba5700da5cca7df2f13c55faf6a56
Stuart <notstuart@gmail.com>
2019-08-07 19:31:11 -0700

    move templated code into a definite implementation

--- general/print.cc
+++ general/print.cc
@@ -68,4 +68,32 @@ namespace ssc
         }
         std::putchar( '\n' );
     }
-}
+    template< typename integral_t >
+    void print_integral_buffer( integral_t * const i_buf, const size_t num_elements )
+    {
+        using std::printf;
+        static constexpr const auto& format_str = []( const size_t size ) {
+                                                      if ( size == sizeof(unsigned char) )
+                                                          return "%2hhx,";
+                                                      else if ( size == sizeof(unsigned short) )
+                                                          return "%2hx,";
+                                                      else if ( size == sizeof( unsigned int) )
+                                                          return "%x,";
+                                                      else if ( size == sizeof(unsigned long) )
+                                                          return "%8lx,";
+                                                      else if ( size == sizeof(unsigned long long) )
+                                                          return "%8llx,";
+                                                      else if ( size == sizeof(size_t) )
+                                                          return "%zx,";
+                                                      else
+                                                          return "";
+                                                  }( sizeof(integral_t) );
+        const integral_t * const alias = reinterpret_cast<const integral_t*>( i_buf );
+        
+        printf( "0x" );
+        for ( size_t i = 0; i < num_elements; ++i )
+            printf( format_str, alias[i] );
+        printf( "\n" );
+    }
+    template void DLL_PUBLIC print_integral_buffer<unsigned char>(unsigned char * const, size_t const);
+}/* ! namespace ssc */
