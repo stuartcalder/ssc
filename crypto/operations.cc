85aa87da9445226864a385aa7e4e18bf24d226ee
Stuart <notstuart@gmail.com>
2019-08-10 23:25:30 -0700

    add ntstatus.h

--- crypto/operations.cc
+++ crypto/operations.cc
@@ -9,6 +9,7 @@
     // Include as the base for win32 crap
     #include <windows.h>
     // Include for definitions of NTSUCCESS
+    #include <ntstatus.h>
     #include <ntdef.h>
 #if 0
     #include <ntstatus.h>
