abc781675f5f53ced96d01c4c360e2706d631326
Stuart <notstuart@gmail.com>
2019-08-12 14:07:18 -0700

    removed unused code

--- general/integers.hh
+++ general/integers.hh
@@ -1,28 +1,18 @@
 #pragma once
+#include <cstddef>
 #include <cstdint>
 
-#if 0
-namespace ssc
-{
-    using u8_t  = std::uint8_t;
-    using u16_t = std::uint16_t;
-    using u32_t = std::uint32_t;
-    using u64_t = std::uint64_t;
-    using i8_t  = std::int8_t;
-    using i16_t = std::int16_t;
-    using i32_t = std::int32_t;
-    using i64_t = std::int64_t;
-}
 #endif
 namespace ssc
 {
     namespace ints
     {
+        // Unsigned integer types
         using u8_t  = std::uint8_t;
         using u16_t = std::uint16_t;
         using u32_t = std::uint32_t;
         using u64_t = std::uint64_t;
-
+        // Signed types
         using i8_t  = std::int8_t;
         using i16_t = std::int16_t;
         using i32_t = std::uint32_t;
