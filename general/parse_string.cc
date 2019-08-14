36152b21da937a7377986321a8b674cda3c24df7
Stuart <notstuart@gmail.com>
2019-08-14 16:36:46 -0700

    add parse_string files

---
+++ general/parse_string.cc
@@ -0,0 +1,18 @@
+#include "parse_string.hh"
+
+namespace ssc
+{
+    bool enforce_integer(std::string & str)
+    {
+        bool success = true;
+        std::string s;
+        for ( char const & ch : str )
+            if ( isdigit( ch ) )
+                s+= ch;
+        if ( s.empty() )
+            success = false;
+        else
+            str = s;
+        return success;
+    }
+}/* ! namespace ssc */
---
+++ general/parse_string.hh
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+#include <ssc/general/symbols.hh>
+
+namespace ssc
+{
+    bool DLL_PUBLIC enforce_integer(std::string & str);
+}/* ! namespace ssc */
