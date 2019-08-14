e8c6e61b712f3887d0bed3cc49d6f7e1bd67b0e4
Stuart <notstuart@gmail.com>
2019-08-13 22:44:47 -0700

    Fixed bug where i32_t erroneously refererd to uint32_t, and i64_t erroneously referred to uint64_t

--- general/integers.hh
+++ general/integers.hh
@@ -14,8 +14,8 @@ namespace ssc
         // Signed types
         using i8_t  = std::int8_t;
         using i16_t = std::int16_t;
-        using i32_t = std::uint32_t;
-        using i64_t = std::uint64_t;
+        using i32_t = std::int32_t;
+        using i64_t = std::int64_t;
     }
     using namespace ints;
 }
