35bb3d140fdcc14e7cf2ef2a1e42905fda8d33c5
Stuart <notstuart@gmail.com>
2019-10-10 23:40:30 -0700

    add header

--- crypto/implementation/cbc_v2.hh
+++ crypto/implementation/cbc_v2.hh
@@ -19,6 +19,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 
 #include <cstring>
 #include <cstdlib>
+#include <string>
 
 #include <ssc/general/symbols.hh>
 #include <ssc/general/integers.hh>
