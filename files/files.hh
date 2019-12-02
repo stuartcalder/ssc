728e6fee18f76440a9fb9039b30ab296f7185a4f
Stuart <notstuart@gmail.com>
2019-12-01 19:57:58 -0800

    inline OS file code

--- files/files.hh
+++ files/files.hh
@@ -17,9 +17,18 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 #include <cstdio>
 #include <string>
 #include <ssc/general/symbols.hh>
+#include <ssc/general/error_conditions.hh>
+#include <ssc/general/integers.hh>
 
-#ifdef __Win64__
+#if    defined (__UnixLike__)
+#	include <sys/types.h>
+#	include <sys/stat.h>
+#	include <unistd.h>
+#	include <fcntl.h>
+#elif  defined (__Win64__)
 #	include <windows.h>
+#else
+#	error "Unsupported OS"
 #endif
 
 namespace ssc {
@@ -32,35 +41,174 @@ namespace ssc {
 #	error "Unsupported platform"
 #endif
 
-	size_t DLL_PUBLIC
-	get_file_size	(OS_File_t const);
+	inline size_t
+	get_file_size (OS_File_t const os_file) {
+		using namespace std;
+#if    defined (__UnixLike__)
+		struct stat stat_struct;
+		if (fstat( os_file, &stat_struct ) == -1)
+			errx( "Error: Unable to fstat file descriptor #%d\n", os_file );
+		return static_cast<size_t>(stat_struct.st_size);
+#elif  defined (__Win64__)
+		LARGE_INTEGER lg_int;
+		if (GetFileSizeEx( os_file, &lg_int ) == 0)
+			errx( "Error: GetFileSizeEx() failed\n" );
+		return static_cast<size_t>(lg_int.QuadPart);
+#else
+#	error "Unsupported platform"
+#endif
+	}
 
-	size_t DLL_PUBLIC
-	get_file_size   (char const * filename);
+	inline size_t
+	get_file_size (char const *filename) {
+		using namespace std;
+#if    defined (__UnixLike__)
+		struct stat s;
+		if (stat( filename, &s) != 0)
+			errx( "Error: Failed to stat() info about %s\n", filename );
+		return static_cast<size_t>(s.st_size);
+#elif  defined (__Win64__)
+		OS_File_t os_file = open_existing_os_file( filename, true );
+		size_t const size = get_file_size( os_file );
+		close( os_file );
+		return size;
+#else
+		size_t num_bytes = 0;
+		FILE * stream = fopen( filename, "rb" );
+		if (stream == nullptr)
+			errx( "Error: Failed to open file %s with fopen()\n", filename );
+		while (fgetc( stream ) != EOF)
+			++num_bytes;
+		if (fclose( stream ) == -1)
+			errx( "Error: Failed to close file %s with fclose()\n", filename );
+		return num_bytes;
+#endif
+	}
 
-	size_t DLL_PUBLIC
-	get_file_size   (std::FILE const * const file);
+	inline size_t
+	get_file_size (std::FILE *file) {
+		using namespace std;
+
+		size_t num_bytes = 0;
+		fpos_t position;
+		if (fgetpos( file, &position ) == -1)
+			errx( "Error: Failed to get file position with fgetpos()\n" );
+		while (fgetc( file ) != EOF)
+			++num_bytes;
+		if (fsetpos( file, &position ) == -1)
+			errx( "Error: Failed to set file position to its original position with fsetpos()\n" );
+		return num_bytes;
+	}
 	
-	bool DLL_PUBLIC
-	file_exists     (char const * filename);
+	inline bool
+	file_exists (char const *filename) {
+		using namespace std;
 
-	void DLL_PUBLIC
-	check_file_name_sanity (std::string const & str, size_t const   min_size);
+		bool exists = false;
+		FILE *test = fopen( filename, "rb" );
+		if (test != nullptr) {
+			fclose( test );
+			exists = true;
+		}
+		return exists;
+	}
 
-	void DLL_PUBLIC
-	enforce_file_existence	(char const * const __restrict filename,
-				 bool const                    force_to_exist,
-				 char const * const __restrict opt_error_msg = nullptr);
+	inline void
+	check_file_name_sanity (std::string const &str, size_t const min_size) {
+		if (str.size() < min_size)
+			errx( "Error: Filename %s must have at least %zu character(s)\n", str.c_str(), min_size );
+	}
 
-	OS_File_t DLL_PUBLIC
-	open_existing_os_file	(char const * filename, bool const readonly);
+	inline void
+	enforce_file_existence (char const *__restrict filename,
+			        bool const             force_to_exist,
+				char const *__restrict opt_error_msg = nullptr)
+	{
+		using namespace std;
 
-	OS_File_t DLL_PUBLIC
-	create_os_file	(char const * filename);
+		bool const exists = file_exists( filename );
+		if (exists) {
+			if (force_to_exist)
+				return;
+		} else {
+			if (!force_to_exist)
+				return;
+		}
+		if (opt_error_msg)
+			errx( opt_error_msg );
+		errx( "Error: The file %s doesn't seem to exist.\n", filename );
+	}
 
-	void DLL_PUBLIC
-	close_os_file    (OS_File_t const os_file); 
+	inline OS_File_t
+	open_existing_os_file (char const *filename, bool const readonly) {
+		using namespace std;
+		enforce_file_existence( filename, true );
+#if    defined (__UnixLike__)
+		int file_d;
+		decltype(O_RDWR) const read_write_rights = (readonly ? O_RDONLY : O_RDWR);
+		if ((file_d = open( filename, read_write_rights, static_cast<mode_t>(0600) )) == -1)
+			errx( "Error: Unable to open existing file %s with open()\n", filename );
+		return file_d;
+#elif  defined (__Win64__)
+		HANDLE file_h;
+		decltype(GENERIC_READ) const read_write_rights = (readonly ? GENERIC_READ : (GENERIC_READ|GENERIC_WRITE));
+		if ((file_h = CreateFileA( filename, read_write_rights, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr )) == INVALID_HANDLE_VALUE)
+			errx( "Error: Unable to open existing file %s with CreateFileA()\n", filename );
+		return file_h;
+#else
+#	error "Unsupported platform"
+#endif
+	}
 
-	void DLL_PUBLIC
-	set_os_file_size (OS_File_t const os_file, size_t const new_size);
+	inline OS_File_t
+	create_os_file (char const *filename) {
+		using namespace std;
+		enforce_file_existence( filename, false );
+#if    defined (__UnixLike__)
+		int file_d;
+		if ((file_d = open( filename, (O_RDWR|O_TRUNC|O_CREAT), static_cast<mode_t>(0600) )) == -1)
+			errx( "Error: Unable to create new file %s with open()\n", filename );
+		return file_d;
+#elif  defined (__Win64__)
+		HANDLE file_h;
+		if ((file_h = CreateFileA( filename, (GENERIC_READ|GENERIC_WRITE), 0, nullptr, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, nullptr )) == INVALID_HANDLE_VALUE)
+			errx( "Error: Unable to create file %s with CreateFileA()\n", filename );
+		return file_h;
+#else
+#	error "Unsupported platform"
+#endif
+	}
+
+	inline void
+	close_os_file (OS_File_t const os_file) {
+		using namespace std;
+#if    defined (__UnixLike__)
+		if (close( os_file ) == -1)
+			errx( "Error: Wasn't able to close file descriptor %d\n", os_file );
+#elif  defined (__Win64__)
+		if (CloseHandle( os_file ) == 0)
+			errx( "Error: Wasn't able to close file handle\n" );
+#else
+#	error "Unsupported platform"
+#endif
+	}
+
+	inline void
+	set_os_file_size (OS_File_t const os_file, size_t const new_size) {
+		using namespace std;
+#if    defined (__UnixLike__)
+		if (ftruncate( os_file, new_size ) == -1)
+			errx( "Error: Failed to set size of file descriptor %d to %zu\n", os_file, new_size );
+#elif  defined (__Win64__)
+		LARGE_INTEGER lg_int;
+		lg_int.QuadPart = static_cast<decltype(large_int.QuadPart)>(new_size);
+		if (SetFilePointerEx( os_file, lg_int, nullptr, FILE_BEGIN ) == 0)
+			errx( "Error: Failed to SetFilePointerEx()\n" );
+		if (SetEndOfFile( os_file ) == 0)
+			errx( "Error: Failed to SetEndOfFile()\n" );
+#else
+#	error "Unsupported platform"
+#endif
+	}
+
 }/* ! namespace ssc */
--- files/os_map.hh
+++ files/os_map.hh
@@ -15,6 +15,16 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 #include <ssc/files/files.hh>
 #include <ssc/general/symbols.hh>
 #include <ssc/general/integers.hh>
+#include <ssc/general/error_conditions.hh>
+
+#if    defined (__UnixLike__)
+#	include <sys/mman.h>
+#elif  defined (__Win64__)
+#	include <windows.h>
+#	include <memoryapi.h>
+#else
+#	error "Unsupported OS"
+#endif
 
 namespace ssc {
 	struct DLL_PUBLIC OS_Map {
@@ -26,12 +36,64 @@ namespace ssc {
 #endif
 	};
 
-	void DLL_PUBLIC
-	map_file   (OS_Map & os_map, bool const readonly);
+	inline void
+	map_file (OS_Map &os_map, bool const readonly) {
+		using namespace std;
+#if    defined (__UnixLike__)
+		decltype(PROT_READ) const readwrite_flag = (readonly ? PROT_READ : (PROT_READ|PROT_WRITE));
+		os_map.ptr = static_cast<u8_t *>(mmap( nullptr, os_map.size, readwrite_flag, MAP_SHARED, os_map.os_file, 0 ));
+		if (os_map.ptr == MAP_FAILED)
+			errx( "Error: Failed to mmap() the file descriptor %d\n", os_map.os_file );
+#elif  defined (__Win64__)
+		decltype(PAGE_READONLY) page_readwrite_flag;
+		decltype(FILE_MAP_READ) map_readwrite_flag;
+		if (readonly) {
+			page_readwrite_flag = PAGE_READONLY;
+			map_readwrite_flag = FILE_MAP_READ;
+		} else {
+			page_readwrite_flag = PAGE_READWRITE;
+			map_readwrite_flag = (FILE_MAP_READ|FILE_MAP_WRITE);
+		}
 
-	void DLL_PUBLIC
-	unmap_file (OS_Map const & os_map);
-	
-	void DLL_PUBLIC
-	sync_map   (OS_Map const & os_map);
+		DWORD high_bits = static_cast<DWORD>(os_map.size >> 32);
+		DWORD low_bits  = static_cast<DWORD>(os_map.size);
+		os_map.win64_filemapping = CreateFileMappingA( os_map.os_file, nullptr, page_readwrite_flag, high_bits, low_bits, nullptr );
+		if (os_map.win64_filemapping == nullptr)
+			errx( "Error: Failed during CreateFileMappingA()\n" );
+		os_map.ptr = static_cast<u8_t *>(MapViewOfFile( os_map.win64_filemapping, map_readwrite_flag, 0, 0, os_map.size ));
+		if (os_map == nullptr)
+			errx( "Error: Failed during MapViewOfFile()\n" );
+#else
+#	error "Unsupported OS"
+#endif
+	}
+
+	inline void
+	unmap_file (OS_Map const &os_map) {
+		using namespace std;
+#if    defined (__UnixLike__)
+		if (munmap( os_map.ptr, os_map.size ) == -1)
+			errx( "Error: Failed to munmap()\n" );
+#elif  defined (__Win64__)
+		if (UnmapViewOfFile( static_cast<LPCVOID>(os_map.ptr) ) == 0)
+			errx( "Error: Failed to UnmapViewOfFile()\n" );
+		close_os_file( os_map.win64_filemapping;
+#else
+#	error "Unsupported OS"
+#endif
+	}
+
+	inline void
+	sync_map (OS_Map const &os_map) {
+#if    defined (__UnixLike__)
+		if (msync( os_map.ptr, os_map.size, MS_SYNC ) == -1)
+			errx( "Error: Failed to msync()\n" );
+#elif  defined (__Win64__)
+		if (FlushViewOfFile( static_cast<LPCVOID>(os_map.ptr), os_map.size ) == 0)
+			errx( "Error: Failed to FlushViewOfFile()\n" );
+#else
+#	error "Unsupported OS"
+#endif
+	}
+
 }/* ! namespace ssc */
