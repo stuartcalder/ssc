85e2bac473480097629193751318dac5513e38bc
Stuart Calder <notstuart@gmail.com>
2020-02-28 15:36:18 -0800

    add abstract.hh

---
+++ general/abstract.hh
@@ -0,0 +1,27 @@
+/*
+Copyright (c) 2019-2020 Stuart Steven Calder
+All rights reserved.
+See accompanying LICENSE file for licensing information.
+*/
+#pragma once
+
+namespace ssc
+{
+	template <typename T>
+		constexpr T Return_Largest (T first, T second)
+		{
+			if (first > second)
+				return first;
+			return second;
+		}
+	template <typename T, typename... Args>
+		constexpr T Return_Largest (T first, T second, Args... args)
+		{
+			T biggest = first;
+			if (second > biggest)
+				biggest = second;
+			if (sizeof...(args) == 0)
+				return biggest;
+			return Return_Largest( biggest, args... );
+		}
+}/*namespace ssc*/
