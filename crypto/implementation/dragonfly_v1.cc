5508d7575f8674b03659f510cbe66c60996b31ed
Stuart Calder <notstuart@gmail.com>
2020-04-19 17:38:10 -0700

    add useful comments

--- crypto/implementation/dragonfly_v1.cc
+++ crypto/implementation/dragonfly_v1.cc
@@ -21,171 +21,178 @@ namespace ssc::crypto_impl::dragonfly_v1
 		      char const         *output_filename)
 	{
 		{// Setup the output map.
-			output_map.size = input_map.size + Visible_Metadata_Bytes;
-			set_os_file_size( output_map.os_file, output_map.size );
-			map_file( output_map, false );
+			output_map.size = input_map.size + Visible_Metadata_Bytes;// Assume the output file's size to be the plaintext size with a visible header.
+			set_os_file_size( output_map.os_file, output_map.size );// Set the output file's size.
+			map_file( output_map, false ); // Memory-map the output file, not readonly.
 		}
 
 		static_assert (Block_Bytes == Threefish_f::Block_Bytes);
-		struct {
+		struct { // Secret data, that will be memory-locked and destroyed after use.
 			CTR_Data_t              ctr_data;
-			union {
+			union { // Only one variant of CATENA will be used (with Phi or without Phi).
 				typename Catena_Strong_f::Data strong;
 				typename Catena_Safe_f::Data   safe;
 			} catena;
+			// One key for encryption, large enough to hold the parity word computed during the Threefish keyschedule generation.
 			u64_t                   enc_key         [Threefish_f::External_Key_Words];
+			// One key for authenticaiton, just large enough for the authentication code in Skein_f::mac().
 			alignas(u64_t) u8_t     auth_key        [Threefish_f::Block_Bytes];
 			typename UBI_f::Data    ubi_data;
+			// Two password buffers. One for the initial input, the second to compare and ensure the intended password is used without error.
 			u8_t                    first_password  [Password_Buffer_Bytes];
 			u8_t                    second_password [Password_Buffer_Bytes];
 			typename CSPRNG_f::Data csprng_data;
+			// A buffer used to temporarily hold random input from the keyboard, that will be hashed into the CSPRNG's seed buffer to strengthen it.
 			u8_t                    entropy_data    [Supplement_Entropy_Buffer_Bytes];
+			// A buffer to hold the output of CATENA in the first `Block_Bytes` of memory, that will then be hashed into `Block_Bytes*2` bytes of memory,
+			// where the first `Block_Bytes` of memory will be used as the encryption key, and the second `Block_Bytes` of memory will be used as the
+			// authentication key.
 			alignas(u64_t) u8_t    hash_output      [Block_Bytes * 2];
 
 		} secret;
-		struct {
+		struct { // Public data, that need not be destroyed after use.
+			// Threefish tweak buffer; large enough to store the parity word during generation of the Threefish keyschedule.
 			u64_t               tf_tweak    [Threefish_f::External_Tweak_Words];
+			// Enough bytes to copy into the nonce portion of the counter-mode's keystream buffer.
 			alignas(u64_t) u8_t ctr_nonce   [CTR_f::Nonce_Bytes];
+			// Enough salt bytes to harden CATENA against adversaries with dedicated hardware.
 			alignas(u64_t) u8_t catena_salt [Salt_Bytes];
 		} pub;
 
-		LOCK_MEMORY (&secret,sizeof(secret));
+		LOCK_MEMORY (&secret,sizeof(secret)); // Lock the secret object into memory; do not swap it to disk.
 
-		int password_size;
+		int password_size; // Store the number of bytes in the password we're about to get.
 
 		{// Obtain the password.
 			password_size = obtain_password<Password_Buffer_Bytes>( secret.first_password,
 					                                        secret.second_password,
 										Password_Prompt,
 										Password_Reentry_Prompt );
-			zero_sensitive( secret.second_password, Password_Buffer_Bytes );
+			zero_sensitive( secret.second_password, Password_Buffer_Bytes ); // Destroy the secondary password buffer; it is no longer necessary.
 		}
-		// Initialize the random number generator.
-		CSPRNG_f::initialize_seed( &secret.csprng_data );
+		CSPRNG_f::initialize_seed( &secret.csprng_data ); // Initialize the random number generator.
 		if( catena_input.supplement_os_entropy ) { // Supplement the RNG's entropy if specified to do so.
 			supplement_entropy( &secret.csprng_data,
 					    secret.entropy_data,
 					    secret.entropy_data + Block_Bytes );
-			zero_sensitive( secret.entropy_data, sizeof(secret.entropy_data) );
+			zero_sensitive( secret.entropy_data, sizeof(secret.entropy_data) ); // Destroy the entropy data.
 		}
 		{// 3 calls to the RNG to generate the tweak, nonce, and salt.
 			CSPRNG_f::get( &secret.csprng_data,
 				       reinterpret_cast<u8_t*>(pub.tf_tweak),
-				       Tweak_Bytes );
+				       Tweak_Bytes ); // Generate 16 bytes of tweak material.
 			CSPRNG_f::get( &secret.csprng_data,
 				       pub.ctr_nonce,
-				       sizeof(pub.ctr_nonce) );
+				       sizeof(pub.ctr_nonce) ); // Generate 32 bytes of nonce material for counter-mode.
 			CSPRNG_f::get( &secret.csprng_data,
 				       pub.catena_salt,
-				       sizeof(pub.catena_salt) );
-			zero_sensitive( &secret.csprng_data, sizeof(secret.csprng_data) );
+				       sizeof(pub.catena_salt) ); // Generate 32 bytes of salt material for CATENA.
+			zero_sensitive( &secret.csprng_data, sizeof(secret.csprng_data) ); // Destroy the CSPRNG data.
 		}
 		{
 			// Generate the catena output, that we will then hash into encryption and authentication keys.
-			if( !catena_input.use_phi ) {
-				// Copy the salt in.
+			if( !catena_input.use_phi ) { // Resistance to cache-timing adversaries branch.
 				memcpy( secret.catena.safe.salt,
 					pub.catena_salt,
-					sizeof(pub.catena_salt) );
-				auto r = Catena_Safe_f::call( &(secret.catena.safe),
-						              secret.hash_output,
-						              secret.first_password,
-						              password_size,
-						              catena_input.g_low,
-						              catena_input.g_high,
-						              catena_input.lambda );
-				if( r != Catena_Safe_f::Return_E::Success ) {
-					zero_sensitive( &secret, sizeof(secret) );
-					UNLOCK_MEMORY (&secret,sizeof(secret));
-					unmap_file( output_map );
-					unmap_file( input_map  );
-					close_os_file( output_map.os_file );
-					close_os_file( input_map.os_file  );
-					remove( output_filename );
+					sizeof(pub.catena_salt) ); // Copy the salt into CATENA's salt buffer.
+				auto r = Catena_Safe_f::call( &(secret.catena.safe), // Use the safe member of the CATENA union.
+						              secret.hash_output,    // Output into the first `Block_Bytes` of secret.hash_output.
+						              secret.first_password, // Process the password buffer.
+						              password_size,         // Process `password_size` bytes of the password buffer.
+						              catena_input.g_low,    // Pass in the lower memory-bound, g_low.
+						              catena_input.g_high,   // Pass in the upper memory-bound, g_high.
+						              catena_input.lambda ); // Pass in the time-cost parameter, lambda.
+				if( r != Catena_Safe_f::Return_E::Success ) { // CATENA failed to allocate memory branch.
+					zero_sensitive( &secret, sizeof(secret) ); // Destroy the secret data.
+					UNLOCK_MEMORY (&secret,sizeof(secret));    // Unlock the secret data.
+					unmap_file( output_map );                  // Unmap the output file.
+					unmap_file( input_map  );                  // Unmap the input file.
+					close_os_file( output_map.os_file );       // Close the output file.
+					close_os_file( input_map.os_file  );       // Close the input file.
+					remove( output_filename );                 // Delete the created output file from the filesystem.
 					errx( "Error: Catena_Safe_f failed with error code %d...\n"
-					      "Allocating too much memory?\n", static_cast<int>(r) );
+					      "Allocating too much memory?\n", static_cast<int>(r) ); // Die.
 				}
-				zero_sensitive( &(secret.catena.safe), sizeof(secret.catena.safe) );
-			} else {
+				zero_sensitive( &(secret.catena.safe), sizeof(secret.catena.safe) ); // Destroy the CATENA safe union member data.
+			} else { // Resistance to massively-parallel adversaries branch.
 				// Copy the salt in.
 				memcpy( secret.catena.strong.salt,
 				        pub.catena_salt,
-					sizeof(pub.catena_salt) );
-				auto r = Catena_Strong_f::call( &(secret.catena.strong),
-					  	                secret.hash_output,
-						                secret.first_password,
-						                password_size,
-						                catena_input.g_low,
-						                catena_input.g_high,
-						                catena_input.lambda );
-				if( r != Catena_Strong_f::Return_E::Success ) {
-					zero_sensitive( &secret, sizeof(secret) );
-					UNLOCK_MEMORY (&secret,sizeof(secret));
-					unmap_file( output_map );
-					unmap_file( input_map  );
-					close_os_file( output_map.os_file );
-					close_os_file( input_map.os_file  );
-					remove( output_filename );
+					sizeof(pub.catena_salt) ); // Copy the salt into CATENA's salt buffer.
+				auto r = Catena_Strong_f::call( &(secret.catena.strong), // Use the strong member of the CATENA union.
+					  	                secret.hash_output,      // Output into the first `Block_Bytes` of secret.hash_output.
+						                secret.first_password,   // Process the password buffer.
+						                password_size,           // Proces `password_size` bytes of the password buffer.
+						                catena_input.g_low,      // Pass in the lower memory-bound, g_low.
+						                catena_input.g_high,     // Pass in the upper memory-bound, g_high.
+						                catena_input.lambda );   // Pass in the time-cost parameter, lambda.
+				if( r != Catena_Strong_f::Return_E::Success ) { // CATENA failed to allocate memory branch.
+					zero_sensitive( &secret, sizeof(secret) ); // Destroy the secret data.
+					UNLOCK_MEMORY (&secret,sizeof(secret));    // Unlock the secret data.
+					unmap_file( output_map );                  // Unmap the output file.
+					unmap_file( input_map  );                  // Unmap the input file.
+					close_os_file( output_map.os_file );       // Close the output file.
+					close_os_file( input_map.os_file  );       // Close the input file.
+					remove( output_filename );                 // Delete the created output file from the filesystem.
 					errx( "Error: Catena_Strong_f failed with error code %d...\n"
-					      "Allocating too much memory?\n", static_cast<int>(r) );
+					      "Allocating too much memory?\n", static_cast<int>(r) ); // Die.
 				}
-				zero_sensitive( &(secret.catena.strong), sizeof(secret.catena.strong) );
+				zero_sensitive( &(secret.catena.strong), sizeof(secret.catena.strong) ); // Destroy the CATENA strong union member data.
 			}
-			zero_sensitive( secret.first_password, sizeof(secret.first_password) );
-			// Hash the output into encryption and authentication keys.
-			static_assert (sizeof(secret.hash_output) == (Block_Bytes * 2));
+			zero_sensitive( secret.first_password, sizeof(secret.first_password) ); // Destroy the password.
+			static_assert (sizeof(secret.hash_output) == (Block_Bytes * 2)); // Hash the output into encryption and authentication keys.
 			Skein_f::hash( &(secret.ubi_data),
-				       secret.hash_output,
-				       secret.hash_output,
-				       Block_Bytes,
-				       (Block_Bytes * 2) );
+				       secret.hash_output, // Output into itself.
+				       secret.hash_output, // Use the output of CATENA as input to Skein.
+				       Block_Bytes,        // Process the first `Block_Bytes` of data, as output by CATENA.
+				       (Block_Bytes * 2) );// Output `Block_Bytes * 2` bytes of pseudorandom data into the buffer.
 			memcpy( secret.enc_key,
 				secret.hash_output,
-				Block_Bytes );
+				Block_Bytes ); // Copy the first Block into the encryption key buffer.
 			memcpy( secret.auth_key,
 				secret.hash_output + Block_Bytes,
-				Block_Bytes );
-			zero_sensitive( secret.hash_output, sizeof(secret.hash_output) );
+				Block_Bytes ); // Copy the second Block into the authentication key buffer.
+			zero_sensitive( secret.hash_output, sizeof(secret.hash_output) ); // Destroy the hash output buffer.
 			Threefish_f::rekey( &(secret.ctr_data.threefish_data),
 					    secret.enc_key,
-					    pub.tf_tweak );
+					    pub.tf_tweak ); // Compute the Threefish_f keyschedule with the encryption key and tweak.
 		}
 		// Setup the public portion of the header
-		u8_t *out = output_map.ptr;
+		u8_t *out = output_map.ptr; // Get a pointer to the beginning of the memory-mapped output file.
 
-		memcpy( out, Dragonfly_V1_ID, sizeof(Dragonfly_V1_ID) );
+		memcpy( out, Dragonfly_V1_ID, sizeof(Dragonfly_V1_ID) ); // Copy the Dragonfly_V1 identifier in.
 		out += sizeof(Dragonfly_V1_ID);
-		(*reinterpret_cast<u64_t*>(out)) = output_map.size;
+		(*reinterpret_cast<u64_t*>(out)) = output_map.size;      // Copy the size of the mapped output file into the mapped output file.
 		out += sizeof(u64_t);
-		(*out++) = catena_input.g_low;
-		(*out++) = catena_input.g_high;
-		(*out++) = catena_input.lambda;
-		(*out++) = catena_input.use_phi;
-		memcpy( out, pub.tf_tweak, Tweak_Bytes );
+		(*out++) = catena_input.g_low;                           // Copy the lower memory-bound in.
+		(*out++) = catena_input.g_high;                          // Copy the upper memory-bound in.
+		(*out++) = catena_input.lambda;                          // Copy the time-cost parameter in.
+		(*out++) = catena_input.use_phi;                         // Copy the phi-usage parameter in.
+		memcpy( out, pub.tf_tweak, Tweak_Bytes );                // Copy the Threefish tweak in.
 		out += Tweak_Bytes;
-		memcpy( out, pub.catena_salt, Salt_Bytes );
+		memcpy( out, pub.catena_salt, Salt_Bytes );              // Copy the CATENA salt in.
 		out += Salt_Bytes;
-		memcpy( out, pub.ctr_nonce, CTR_f::Nonce_Bytes );
+		memcpy( out, pub.ctr_nonce, CTR_f::Nonce_Bytes );        // Copy the CTR mode nonce in.
 		out += CTR_f::Nonce_Bytes;
 		// Setup the to-be-encrypted portion of the header.
 		{
 			_CTIME_CONST (int) Zeroes = sizeof(u64_t) * 2;
 			{
-				u8_t zeroes [Zeroes] = { 0 };
+				u8_t zeroes [Zeroes] = { 0 }; // 16 0x00 bytes.
 				CTR_f::set_nonce( &(secret.ctr_data),
-						  pub.ctr_nonce );
+						  pub.ctr_nonce ); // Set the nonce of the Counter-Mode keystream buffer.
 				// The encrypted, reserved portion of the header.
 				CTR_f::xorcrypt( &(secret.ctr_data),
 						 out,
 						 zeroes,
-						 Zeroes );
+						 Zeroes ); // Encrypt the zeroes appending them to the end of the visible header, just before the payload.
 			}
 			out += Zeroes;
 			CTR_f::xorcrypt( &(secret.ctr_data),
 					 out,
 					 input_map.ptr,
 					 input_map.size,
-					 Zeroes );
+					 Zeroes ); // Encrypt the input file, outputting the payload.
 			out += input_map.size;
 		}
 		{
@@ -194,7 +201,7 @@ namespace ssc::crypto_impl::dragonfly_v1
 				      output_map.ptr,
 				      secret.auth_key,
 				      MAC_Bytes,
-				      output_map.size - MAC_Bytes );
+				      output_map.size - MAC_Bytes ); // Authenticate the 
 		}
 		zero_sensitive( &secret, sizeof(secret) );
 
@@ -361,6 +368,8 @@ namespace ssc::crypto_impl::dragonfly_v1
 		}
 		u64_t plaintext_size = output_map.size;
 		{
+			CTR_f::set_nonce( &(secret.ctr_data),
+					  pub.ctr_nonce );
 			u64_t padding_bytes;
 			CTR_f::xorcrypt( &secret.ctr_data,
 					 reinterpret_cast<u8_t*>(&padding_bytes),
@@ -430,18 +439,31 @@ namespace ssc::crypto_impl::dragonfly_v1
 		header.id[ sizeof(header.id) - 1 ] = '\0';
 		fprintf( stdout, "File Header ID : %s\n", reinterpret_cast<char*>(header.id) );
 		fprintf( stdout, "File Size      : %zu\n", header.total_size );
+		fprintf( stdout, "Garlic Low:  %u\n", static_cast<unsigned int>(header.g_low) );
+		fprintf( stdout, "Garlic High: %u\n", static_cast<unsigned int>(header.g_high) );
+		fprintf( stdout, "Lambda:      %u\n", static_cast<unsigned int>(header.lambda) );
+#if 0
 		fprintf( stdout, "Garlic Low: %c\n", header.g_low );
 		fprintf( stdout, "Garlic High: %c\n", header.g_high );
 		fprintf( stdout, "Lambda: %c\n", header.lambda );
+#endif
+#if 0
+		fprintf( stdout, "Garlic Low\n");
+		print_integral_buffer<u8_t>( &header.g_low, sizeof(header.g_low) );
+		fprintf( stdout, "Garlic High:\n" );
+		print_integral_buffer<u8_t>( &header.g_high, sizeof(header.g_high) );
+		fprintf( stdout, "Lambda:\n" );
+		print_integral_buffer<u8_t>( &header.lambda, sizeof(header.lambda) );
+#endif
 		if( !header.use_phi )
 			fprintf( stdout, "The Phi function is not used.\n" );
 		else
 			fprintf( stdout, "The Phi function is used!\n" );
-		fputs(           "Threefish Tweak : ", stdout );
+		fputs(           "Threefish Tweak :\n", stdout );
 		print_integral_buffer<u8_t>( header.tweak, sizeof(header.tweak) );
-		fputs(           "Catena Salt : ", stdout );
+		fputs(         "\nCatena Salt :\n", stdout );
 		print_integral_buffer<u8_t>( header.salt, sizeof(header.salt) );
-		fputs(           "CTR Salt : ", stdout );
+		fputs(         "\nCTR Nonce:\n", stdout );
 		print_integral_buffer<u8_t>( header.nonce, sizeof(header.nonce) );
 	}
 }/* ~ namespace ssc::crypto_impl::dragonfly_v1 */
